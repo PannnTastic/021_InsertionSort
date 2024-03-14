5ba8917e8645fb56818c77fa1da0441807673ba3
PannnTastic <pannntastic@gmail.com>
2024-03-14 17:34:42 +0700

    mengisi procedure insertionSort dengan algoritma Insertion Sort

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -4,6 +4,19 @@ using namespace std;
 void insertionSort() {
     int i, n, j, temp;
     int arr[9];
+
+    for (i = 1; i <= n - 1; i++) {  
+
+        temp = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp) {
+
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
 }
 
 int main()

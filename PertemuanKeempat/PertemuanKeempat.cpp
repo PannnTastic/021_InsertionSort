d07e22afedab36830a0580d6a8f80ce63db595a4
PannnTastic <pannntastic@gmail.com>
2024-03-14 17:40:05 +0700

    Menambahkan procedure input dan membuat variabel global

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int n;
+int arr[9];
+
+void input() {//prosedur untuk input
+    while (true) {
+        cout << "Masukkan Banyak elemen Pada Array : ";//output ke layar
+        cin >> n;//input ke pengguna
+        if (n <= 20)//jika n kurang dari atau samadengan 20
+            break;//keluar dari loop
+
+        else {
+            cout << "\nArray hanya dapat menampung 20 elemen\n";//output jikaelemen yang dimasukkan lebih dari 20
+        }
+    }
+
+    cout << endl;//output baris kosong
+    cout << "=================\n";
+    cout << "Masukkan Elemen Array\n";//output ke layar
+    cout << "================\n";
+
+    for (int i = 0; i < n; i++) {//looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";//output ke layar
+        cin >> arr[i];//input pengguna
+    }
+
+}
+
 void insertionSort() {
-    int i, n, j, temp;
-    int arr[9];
+    int i, j, temp;
+    
 
     for (i = 1; i <= n - 1; i++) {  
 

2285adf3adc913e705dcb3dcad4cf264e51639eb
PannnTastic <pannntastic@gmail.com>
2024-03-14 17:48:12 +0700

    menambahkan komentar

--- PertemuanKeempat/PertemuanKeempat.cpp
+++ PertemuanKeempat/PertemuanKeempat.cpp
@@ -28,21 +28,21 @@ void input() {//prosedur untuk input
 
 }
 
-void insertionSort() {
+void insertionSort() { //procedure untuk insertion sort
     int i, j, temp;
     
 
-    for (i = 1; i <= n - 1; i++) {  
+    for (i = 1; i <= n - 1; i++) {  //step 1
 
-        temp = arr[i];
-        j = i - 1;
+        temp = arr[i];//step 2
+        j = i - 1;//step 3
 
-        while (j >= 0 && arr[j] > temp) {
+        while (j >= 0 && arr[j] > temp) {//step 4
 
-            arr[j + 1] = arr[j];
-            j--;
+            arr[j + 1] = arr[j];//step 4a
+            j--;//step 4b
         }
-        arr[j + 1] = temp;
+        arr[j + 1] = temp;//step 5
     }
 }
 
